1bc72feb9719f537117eba1bdc63690d5210b991
Mohamed khedr <120657300+Mohamed-5der@users.noreply.github.com>
2023-11-27 21:42:10 +0200

    new

--- app/src/main/java/com/example/kotlinproject/TaskKotlinOpp4.kt
+++ app/src/main/java/com/example/kotlinproject/TaskKotlinOpp4.kt
@@ -1,46 +1,20 @@
 package com.example.kotlinproject
 
+import java.util.*
+
 fun main(){
- println("Choose Game:")
- println("1. Football  2. Basketball  3. Karate  4. Chess")
+    val scanner = Scanner(System.`in`)
+    println("Enter the weight of the watermelon : " )
+    var w :Int = scanner.nextInt()
 
- val choose = readLine()?.toInt() ?:1
- printGame(choose)
-}
-fun printGame (choose :Int){
-   when (choose) {
-     1 ->{
-      val foot = Football()
-      foot.printInformationGame()
-     }
-    2 ->{
-     val bask = Basketball()
-     bask.printInformationGame()
+    if (w % 2 == 0 && w >= 1 && w <=100) {
+        mkldnkln
+        esklgjr;
+        print("yes")
+    }else {
+        println("No")
     }
-    3 ->{
-     val karate = Karate()
-     karate.printInformationGame()
-    }
-    4  ->{
-     val chess = Chess()
-     chess.printInformationGame()
-    }
-   }
-}
-open class Game(val nameGame :String,val GamePlayersNumber :Int,val GameBall:String,val GameStatus :String){
-     fun printInformationGame(){
-        println("Name Game : $nameGame GamePlayersNum : $GamePlayersNumber GameBall : $GameBall  GameStatus : $GameStatus")
-     }
-}
- class Football :Game ( "Football",  11 ,  "have ball" , "collective"){
 
 }
- class Basketball :Game (   "Basketball",  5 , "have ball", "collective"){
 
- }
- class Karate : Game ("Karate", 1, "not have ball", "individually"){
-
- }
- class Chess :Game( "Chess", 1, "not have ball", "individually"){
- }
 
